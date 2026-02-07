24be980d7365e1fdea9bed691a5d1640adbe43f9
ChanchalAgrawalMiku <113780648+ChanchalAgrawalMiku@users.noreply.github.com>
2026-02-07 15:09:47 +0200

    Update music streaming URL in config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.youtube.com/watch?v=iAIBF2ngbWY&list=RDiAIBF2ngbWY&start_radio=1", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/de5x1coib/video/upload/v1770469368/ANIMAL_Pehle_Bhi_Main_lecxyp.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
